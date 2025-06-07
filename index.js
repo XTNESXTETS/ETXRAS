357d32933996dcbe56df748434c9aae6799e0e05
XTNESXTETS <xnrltweaks@gmail.com>
2025-06-07 11:38:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
@@ -14,36 +13,34 @@ app.use(bodyParser.json());
 
 // Save User Data
 app.post('/FirebaseProxy/saveUserData', async (req, res) => {
-try {
-const userData = req.body;
-if (!userData.UserId) {
-return res.status(400).send({ error: 'Missing UserId' });
-}
+  try {
+    const userData = req.body;
+    if (!userData.UserId) {
+      return res.status(400).send({ error: 'Missing UserId' });
+    }
 
-const url = `${firebaseDbUrl}users/${userData.UserId}.json?auth=${firebaseDbSecret}`;
-const response = await axios.put(url, userData);
-res.send({ success: true, data: response.data });
-} catch (err) {
-console.error(err);
-res.status(500).send({ error: 'Failed to save user data' });
-}
+    const url = `${firebaseDbUrl}users/${userData.UserId}.json?auth=${firebaseDbSecret}`;
+    const response = await axios.put(url, userData);
+    res.send({ success: true, data: response.data });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'Failed to save user data' });
+  }
 });
 
 // Get User Data
 app.get('/FirebaseProxy/getUserData/:userId', async (req, res) => {
-try {
-const userId = req.params.userId;
-        const url = `${firebaseDbUrl}users/${userData.UserId}.json?auth=${firebaseDbSecret}`;
-        const url = `${firebaseDbUrl}users/${userId}.json?auth=${firebaseDbSecret}`;
-const response = await axios.get(url);
-res.send(response.data);
-} catch (err) {
-console.error(err);
-res.status(500).send({ error: 'Failed to get user data' });
-}
+  try {
+    const userId = req.params.userId;
+    const url = `${firebaseDbUrl}users/${userId}.json?auth=${firebaseDbSecret}`;
+    const response = await axios.get(url);
+    res.send(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'Failed to get user data' });
+  }
 });
 
 app.listen(port, () => {
-    console.log(\`FirebaseProxy backend running on port \${port}\`);
-    console.log(`FirebaseProxy backend running on port ${port}`);
+  console.log(`FirebaseProxy backend running on port ${port}`);
 });
